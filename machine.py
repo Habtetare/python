bf911aae4036dfd9be0b82a1bcb3ca42e595ed9f
Habtetare <121849266+Habtetare@users.noreply.github.com>
2025-04-03 14:20:27 +0300

    commit

--- machine.py
+++ machine.py
@@ -1,10 +1,12 @@
 emotion = "v.v"
 
 def main():
-    #global emotion
+    global emotion
     say("Is anyone there ?")
     emotion = ":D"
     say("Oh, hi!")
 
 def say(phrase):
     print(phrase + "" + emotion)
+
+main()
